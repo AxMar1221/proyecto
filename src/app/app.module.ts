9753229621d50f000ee9aac30e0c47c9cb7ae9e7
Tachidito <85906328+AxMar1221@users.noreply.github.com>
2023-01-16 11:54:27 -0600

    bases

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { EmployeesComponent } from './pages/employees/employees.component';
+import { GroupsComponent } from './pages/groups/groups.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
